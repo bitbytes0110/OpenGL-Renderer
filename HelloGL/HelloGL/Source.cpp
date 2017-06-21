eeb975f9a5039d7ee665e4ea028c456611d1791f
bitbytes0110 <bitbytes0110@gmail.com>
2017-06-22 00:13:06 +0530

    Loading and sampling textures from images (using stb_image library)

--- HelloGL/HelloGL/Source.cpp
+++ HelloGL/HelloGL/Source.cpp
@@ -1,6 +1,8 @@
 #include<glad\glad.h>
 #include<GLFW\glfw3.h>
 
+#include"stb_image.h"
+
 #include"Macros.h"
 #include"Shader.h"
 #include<iostream>
@@ -10,6 +12,8 @@ void SetWireframe(bool bValue);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void SetWireframe(bool bValue);
 void processInput(GLFWwindow * window);
+
+float mixValue = 0.4f;
 
 int main()
 {
@@ -51,17 +55,22 @@ int main()
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
-		// positions         // colors
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
-
+	unsigned int indices[] = {
+		0, 1, 2, // first triangle
+		0, 2, 3  // second triangle
+	};
 	//	Vertex Buffer Object		( Stores all vertex data )
-	unsigned int VBO, VAO;
+	unsigned int VBO, EBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
@@ -69,20 +78,101 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// for indices data 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// Setting the vertex attributes (how the vertex data should be interpreted)
 	// position attribute
-	glVertexAttribPointer(0,3, GL_FLOAT,GL_FALSE,6*(sizeof(GL_FLOAT)),(void*)0);
+	glVertexAttribPointer(0,3, GL_FLOAT,GL_FALSE, 8*(sizeof(GL_FLOAT)),(void*)0);
 	glEnableVertexAttribArray(0);
 
 	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * (sizeof(GL_FLOAT)), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	// Texture attribute
+	glVertexAttribPointer(2, 2,GL_FLOAT, GL_FALSE, 8 * (sizeof(GL_FLOAT)), (void*)(6*sizeof(float)));
+	glEnableVertexAttribArray(2);
 	
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	// glBindVertexArray(0);
 
+	stbi_set_flip_vertically_on_load(true);	// tell stb_image.h to flip loaded texture's on the y-axis.
+	// load and create a texture 
+	// -------------------------
+	unsigned int myTexture1, myTexture2;
 
+	// TEXTURE 2 
+	// ---------
+
+	glGenTextures(1, &myTexture1);
+	glBindTexture(GL_TEXTURE_2D, myTexture1);
+
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	// load the image data
+	int width, height, nChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nChannels, 0);
+
+
+	//	Generate texture
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		LOG("FAILED TO LOAD TEXTURE");
+	}
+
+	stbi_image_free(data);
+	
+	// TEXTURE 2 
+	// ---------
+
+	glGenTextures(1, &myTexture2);
+	glBindTexture(GL_TEXTURE_2D, myTexture2);
+
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load the image data
+
+	int width2, height2, nChannels2;
+	unsigned char* data2 = stbi_load("face.png", &width2, &height2, &nChannels2, 0);
+
+	//	Generate texture
+	if (data2)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		LOG("FAILED TO LOAD TEXTURE");
+	}
+
+	stbi_image_free(data2);
+	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+	// -------------------------------------------------------------------------------------------
+	ourShaderProgram.Use();// don't forget to activate/use the shader before setting uniforms!
+	ourShaderProgram.SetInt("WoodTexture", 0);
+	ourShaderProgram.SetInt("FaceTexture", 1);
+
+	ourShaderProgram.SetFloat("mixValue", mixValue);
+
+
+	//SetWireframe(TRUE);
 	// Game Loop
 	// ----------------
 	while (!glfwWindowShouldClose(window))	
@@ -93,15 +183,22 @@ int main()
 
 		// Render
 		// ------
-		glClearColor(0.2f,0.3f, 0.5f, 1.0f);
+		glClearColor(0.05f,0.05f, 0.05f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
-		//	Render the Triangle
+		// bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, myTexture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, myTexture2);
 
+		ourShaderProgram.SetFloat("mixValue", mixValue);
+
+		//	Render Container
 		ourShaderProgram.Use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 				
 		// End of Rendering Code
 
@@ -141,6 +238,19 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, TRUE);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		mixValue = mixValue + 0.005f;
+		if (mixValue > 1)
+			mixValue = 1.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		mixValue = mixValue - 0.005f;
+		if (mixValue < 0)
+			mixValue = 0.0f;
+	}
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
